25d712eebe528cc356a5ca77bde87c512036b6fd
Tania <taniyhamail@gmail.com>
2024-04-26 21:55:36 +0300

    feat: added loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar/NavBar'
+import Title from './components/Title/Title'
+import styles from './page.module.css'
+
+export default function Loading() {
+  return (
+    <main className={styles.main}>
+      <NavBar full={true} />
+      <Title text={'Loading...'} />
+    </main>
+  )
+}
